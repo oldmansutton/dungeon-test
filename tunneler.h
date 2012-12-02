1172f168dfacef71e598b856ff59751db970ad9b
oldmansutton <oldmansutton@gmail.com>
2012-12-02 14:07:57 -0500

    Add tunneler class

---
+++ tunneler.h
@@ -0,0 +1,37 @@
+// tunneler.h
+//
+// Copyright (C) 2012 - Ryan Sutton
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef TUNNELER_H
+#define TUNNELER_H
+
+typedef struct 
+{
+	int xCoord;
+	int yCoord;
+	int xDir;
+	int yDir;
+	int lifetime;
+	int age;
+	int turnChance;
+	int tunnelerSpawn;
+	int roomerSpawn;
+	int builderSpawn;
+} Tunneler;
+	
+extern void tunneler_rand(Tunneler *inTunneler);
+
+#endif 
