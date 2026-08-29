497a2c4ab8877f12ccdee4de835a8775d8a8cf0f
oldmansutton <oldmansutton@gmail.com>
2026-08-29 22:47:07 +0000

    memememememe

--- attributes.h
+++ attributes.h
@@ -12,17 +12,17 @@
 typedef struct {
     // BASE ATTRIBUTES
     // Physical
-    unsigned int strength;      // power: melee/carry
-    unsigned int dexterity;     // finesse: precision/hit/dodge/speed
-    unsigned int constitution;  // resistance: hp/regen/poisons
+    int strength;      // power: melee/carry
+    int dexterity;     // finesse: precision/hit/dodge/speed
+    int constitution;  // resistance: hp/regen/poisons
     // Mental
-    unsigned int intelligence;  // power: spell dmg/mana/learning
-    unsigned int wisdom;        // finesse: perception/tracks/mana regen
-    unsigned int resolve;       // resistance: willpower/focus
+    int intelligence;  // power: spell dmg/mana/learning
+    int wisdom;        // finesse: perception/tracks/mana regen
+    int resolve;       // resistance: willpower/focus
     // Social
-    unsigned int presence;      // power: intimidate/leader
-    unsigned int charisma;      // finesse: barter/deception/persuasion
-    unsigned int nerve;         // resistance: sanity/bravery/fear resist
+    int presence;      // power: intimidate/leader
+    int charisma;      // finesse: barter/deception/persuasion
+    int nerve;         // resistance: sanity/bravery/fear resist
     // TRAIT ACTIVATED ATTRIBUTES
     int luck; // modifies roll outcomes +/-, loot drops, crits, etc.
 } Attributes;
